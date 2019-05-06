8640ab995bbbd310af0344aa3f0861691931121f
Sutton <suco3693@gmail.com>
2019-05-06 15:37:19 -0700

    Addition of api call to authenticjobs api but commented out for mock data for testing

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const request = require("request");
 const controller = require("./controller");
+const authJobs = require("../authJobs");
+const mockData = require("./sampleData.json");
 const port = process.env.PORT || 5000;
 const server = express();
 
@@ -14,6 +17,18 @@ server.use(bodyParser.urlencoded( {extended : true}));
 
 server.use('/api/keywords/', controller.getAll);
 
+
+server.use('/api/searchJobs/', (req, res)=>{
+  var keywords = req.body.keywords.join();
+  res.send(JSON.stringify(mockData))
+  // request(`https://authenticjobs.com/api/?api_key=${authJobs.key}&method=aj.jobs.search&keywords=${keywords}&format=json&perpage=25`,(err,responce,body)=>{
+  //   if(err){
+  //     res.send(err);
+  //   } else {
+  //     res.send(JSON.stringify(body));
+  //   }
+  // })
+})
 
 
 server.listen(port, ()=>{
