ce252e1d881cecfbb9eb2a42be12ebb9b389353f
Sutton <suco3693@gmail.com>
2019-05-06 19:43:07 -0700

    Switched authenticjobs api back on for final build

--- server/server.js
+++ server/server.js
@@ -20,14 +20,14 @@ server.use('/api/keywords/', controller.getAll);
 
 server.use('/api/searchJobs/', (req, res)=>{
   var keywords = req.body.keywords.join();
-  res.send(JSON.stringify(mockData));
-  // request(`https://authenticjobs.com/api/?api_key=${authJobs.key}&method=aj.jobs.search&keywords=${keywords}&format=json&perpage=25`,(err,responce,body)=>{
-  //   if(err){
-  //     res.send(err);
-  //   } else {
-  //     res.send(body);
-  //   }
-  // })
+  // res.send(JSON.stringify(mockData));
+  request(`https://authenticjobs.com/api/?api_key=${authJobs.key}&method=aj.jobs.search&keywords=${keywords}&format=json&perpage=25`,(err,responce,body)=>{
+    if(err){
+      res.send(err);
+    } else {
+      res.send(body);
+    }
+  })
 })
 
 
